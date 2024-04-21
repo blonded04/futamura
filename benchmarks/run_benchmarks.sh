d2af51a07a47a569bc2195294573b281ed15e016
blonded04 <generalretcher@gmail.com>
2024-04-21 17:51:44 +0300

    added ASM interpreter

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -4,12 +4,12 @@ for file in *.lama; do
     if [ -f "$file" ]; then
         # execute lamac version:
         lamac "${file}"
-        echo "${file%} lamac:"
+        echo "* ${file%} lamac:"
         /usr/bin/time --format "\treal: %E" taskset -c 0 ./${file%.*}
-        # # execute byterun version:
-        # lamac -bc "${file%.lama}"
-        # echo "${file%} byterun:"
-        # /usr/bin/time --format "\treal: %E" taskset -c 0 ../byterun/byterun ${file%}.bc
+        # execute byterun version:
+        lamac -b "${file}"
+        echo "* ${file%} byterun:"
+        /usr/bin/time --format "\treal: %E" taskset -c 0 ./../byterun/byterun ${file%.*}.bc
         # # execute spec version:
         # # TODO: add spec version here!!!
         # echo "${file%} spec:"
